2d111f746ac8b775dfe46a7994ad0c653d12c8e0
yanglong <yanglong@rijin.com>
2015-07-16 13:25:00 +0800

    fix index out of range bug

--- hooks/file/hook.go
+++ hooks/file/hook.go
@@ -74,7 +74,10 @@ func getMessage(entry *logrus.Entry) (message string, err error) {
 	for k, v := range entry.Data {
 		fields = fields + fmt.Sprintf("%s:%v;", k, v)
 	}
-	call := fmt.Sprintf("%s:%d[%s]", file, lineNumber, fields[:len(fields)-1])
+	if len(fields) > 0 {
+		fields = fields[:len(fields)-1]
+	}
+	call := fmt.Sprintf("%s:%d[%s]", file, lineNumber, fields)
 	message = fmt.Sprintf("%s\n%s", entry.Message, call)
 	return
 }
